859f9196db9810ee30d3054678b469ca58afa7c0
{minakawa <minakawa.webcreate@gmail.com>
2023-10-30 23:23:48 +0900

    初めて

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', function () {
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+
+const navLinks = document.querySelectorAll(".nav-items__item");
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
+        ham.classList.remove("active");
+        nav.classList.remove("active");
+    });
+});
